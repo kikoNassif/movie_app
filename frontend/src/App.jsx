3495e64fd7c0b00ef10f9fbe31592eefd7ac2b94
Karim Nassif <Ktamer2003@gmail.com>
2025-04-05 23:50:22 +0400

    set up home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+import {Routes, Route} from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+        <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/favorites' element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import MovieCard from '../components/MovieCard'
+
+function Home() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    {id: 1, title: "John Wick", release_date: "2020"},
+    {id: 2, title: "Terminator", release_date: "1999"},
+    {id: 3, title: "The Matrix", release_date: "1998"},
+  ]
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    alert(searchQuery)
+  }
+
+  return (
+    <div className='home'>
+      <form onSubmit={handleSearch} className='search-form'>
+        <input 
+        type="text" 
+        placeholder='Search for movies ...'
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type='submit' placeholder='search-button'>Search</button>
+      </form>
+
+      <div className='moveis-grid'>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}/>
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
